8761019fd8f2065a8cb7b0e5e114585b6d181de1
Daniel Brazel <dbrazel4@gmail.com>
2018-04-13 14:33:22 -0400

    User class partially built, Ingredient and Allergen fully built

--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,16 @@
 require_relative '../config/environment.rb'
 
+wheat = Ingredient.new("wheat")
+onion = Ingredient.new("onion")
+mud = Ingredient.new("mud")
+paul = User.new("paul")
+bob = User.new("bob")
+mary = User.new("mary")
+trudy = User.new("trudy")
+Allergen.new(wheat, bob)
+Allergen.new(wheat, mary)
+Allergen.new(wheat, trudy)
+Allergen.new(onion, paul)
+Allergen.new(mud, bob)
+
 binding.pry
